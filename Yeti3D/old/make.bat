ec6db60523c3d0d0e2ec226cfa3d73c1b148870d
EXL <exlmotodev@gmail.com>
2023-04-20 01:14:32 +0700

    [Yeti3D]: Start implementing P2K port

--- Yeti3D/old/make.bat
+++
@@ -1,9 +0,0 @@
-del game.gba
-
-set path=C:\devkitadv\bin;%path%
-
-g++ -marm -mthumb-interwork -mlong-calls -O3 -o game.elf data.cpp yeti.cpp draw.cpp main.cpp
-
-objcopy -O binary game.elf game.gba
-
-del game.elf
