63a686d74ef7b3db43b1f03a6f4e998f73553708
EXL <exlmotodev@gmail.com>
2023-08-16 16:32:11 +0700

    [Yeti3D]: Add exit fixes for V300

--- Yeti3D/main_p2k.c
+++ Yeti3D/main_p2k.c
@@ -396,7 +396,11 @@ static UINT32 ProcessKeyboard(EVENT_STACK_T *ev_st, APPLICATION_T *app, UINT32 k
 	switch (key) {
 		case MULTIKEY_0:
 		case MULTIKEY_SOFT_LEFT:
+#if defined(FTR_V300)
+			AFW_AddEvNoD(ev_st, EV_DONE);
+#else
 			app->exit_status = TRUE;
+#endif
 			break;
 		case MULTIKEY_1:
 			g_keyboard[E_KEY_LOOK_DOWN] = pressed;
