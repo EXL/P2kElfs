43cca856ecbca09cd344ec6806f8d67668f4482c
EXL <exlmotodev@gmail.com>
2023-10-01 14:02:03 +0700

    [Benchmark]: Add VibroSignal utility project

---
+++ Benchmark/VibroSignal/VibroSignal.c
@@ -0,0 +1,490 @@
+/*
+ * About:
+ *
+ * Author:
+ *   EXL
+ *
+ * License:
+ *   MIT
+ *
+ * Application type:
+ *   GUI + UIS Canvas
+ *
+ * Additional information:
+ *
+ */
+
+#include <loader.h>
+#include <apps.h>
+#include <uis.h>
+#include <canvas.h>
+#include <mem.h>
+#include <utilities.h>
+#include <dl.h>
+#include <tasks.h>
+
+#define TIMER_FAST_TRIGGER_MS             (1)
+
+typedef enum {
+	APP_STATE_ANY,
+	APP_STATE_INIT,
+	APP_STATE_MAIN,
+	APP_STATE_MAX
+} APP_STATE_T;
+
+typedef enum {
+	APP_TIMER_EXIT = 0x0001,
+	APP_TIMER_EXIT_FAST
+} APP_TIMER_T;
+
+typedef enum {
+	BENCHMARK_MAIN_SCREEN
+} BENCHMARK_STATE_T;
+
+typedef struct {
+	APPLICATION_T app;
+
+	GRAPHIC_REGION_T area;
+} APP_INSTANCE_T;
+
+#if defined(EP1)
+UINT32 Register(const char *elf_path_uri, const char *args, UINT32 ev_code); /* ElfPack 1.x entry point. */
+#elif defined(EP2)
+ldrElf *_start(WCHAR *uri, WCHAR *arguments);                                /* ElfPack 2.x entry point. */
+#elif defined(EPMCORE)
+UINT32 ELF_Entry(ldrElf *elf, WCHAR *arguments);                             /* ElfPack M*CORE entry point. */
+#endif
+
+static UINT32 ApplicationStart(EVENT_STACK_T *ev_st, REG_ID_T reg_id, void *reg_hdl);
+static UINT32 ApplicationStop(EVENT_STACK_T *ev_st, APPLICATION_T *app);
+
+static UINT32 HandleStateEnter(EVENT_STACK_T *ev_st, APPLICATION_T *app, ENTER_STATE_TYPE_T state);
+static UINT32 DeleteDialog(APPLICATION_T *app);
+
+static UINT32 HandleEventKeyPress(EVENT_STACK_T *ev_st, APPLICATION_T *app);
+static UINT32 HandleEventKeyRelease(EVENT_STACK_T *ev_st, APPLICATION_T *app);
+static UINT32 HandleEventTimerExpired(EVENT_STACK_T *ev_st, APPLICATION_T *app);
+
+static UINT32 PaintAll(EVENT_STACK_T *ev_st, APPLICATION_T *app, BENCHMARK_STATE_T state);
+
+static const char g_app_name[APP_NAME_LEN] = "Benchmark";
+
+static const COLOR_T g_color_background   = { 0xBB, 0xAD, 0xA0, 0xFF };
+
+#if defined(EP2)
+static ldrElf g_app_elf;
+#elif defined(EPMCORE)
+static ldrElf *g_app_elf = NULL;
+#endif
+
+static EVENT_HANDLER_ENTRY_T g_state_any_hdls[] = {
+	{ EV_REVOKE_TOKEN, APP_HandleUITokenRevoked },
+	{ EV_TIMER_EXPIRED, HandleEventTimerExpired },
+	{ STATE_HANDLERS_END, NULL }
+};
+
+static EVENT_HANDLER_ENTRY_T g_state_init_hdls[] = {
+	{ EV_GRANT_TOKEN, APP_HandleUITokenGranted },
+	{ STATE_HANDLERS_END, NULL }
+};
+
+static EVENT_HANDLER_ENTRY_T g_state_main_hdls[] = {
+	{ EV_DONE, ApplicationStop },
+	{ EV_DIALOG_DONE, ApplicationStop },
+	{ EV_INK_KEY_PRESS, HandleEventKeyPress },
+	{ EV_INK_KEY_RELEASE, HandleEventKeyRelease },
+	{ STATE_HANDLERS_END, NULL }
+};
+
+static const STATE_HANDLERS_ENTRY_T g_state_table_hdls[] = {
+	{ APP_STATE_ANY, NULL, NULL, g_state_any_hdls },
+	{ APP_STATE_INIT, NULL, NULL, g_state_init_hdls },
+	{ APP_STATE_MAIN, HandleStateEnter, NULL, g_state_main_hdls }
+};
+
+#if defined(EP1)
+UINT32 Register(const char *elf_path_uri, const char *args, UINT32 ev_code) {
+	UINT32 status;
+	UINT32 ev_code_base;
+
+	ev_code_base = ev_code;
+
+	status = APP_Register(&ev_code_base, 1, g_state_table_hdls, APP_STATE_MAX, (void *) ApplicationStart);
+
+	LdrStartApp(ev_code_base);
+
+	return status;
+}
+#elif defined(EP2)
+ldrElf *_start(WCHAR *uri, WCHAR *arguments) {
+	UINT32 status;
+	UINT32 ev_code_base;
+	UINT32 reserve;
+
+	if (ldrIsLoaded(g_app_name)) {
+		cprint("Benchmark: Error! Application has already been loaded!\n");
+		return NULL;
+	}
+
+	status = RESULT_OK;
+	ev_code_base = ldrRequestEventBase();
+	reserve = ev_code_base + 1;
+	reserve = ldrInitEventHandlersTbl(g_state_any_hdls, reserve);
+	reserve = ldrInitEventHandlersTbl(g_state_init_hdls, reserve);
+	reserve = ldrInitEventHandlersTbl(g_state_main_hdls, reserve);
+
+	status |= APP_Register(&ev_code_base, 1, g_state_table_hdls, APP_STATE_MAX, (void *) ApplicationStart);
+
+	status |= ldrSendEvent(ev_code_base);
+	g_app_elf.name = (char *) g_app_name;
+
+	return (status == RESULT_OK) ? &g_app_elf : NULL;
+}
+#elif defined(EPMCORE)
+UINT32 ELF_Entry(ldrElf *elf, WCHAR *arguments) {
+	UINT32 status;
+	UINT32 reserve;
+
+	status = RESULT_OK;
+	g_app_elf = elf;
+	g_app_elf->name = (char *) g_app_name;
+
+	if (ldrIsLoaded(g_app_elf->name)) {
+		PFprintf("%s: Application already loaded.\n", g_app_elf->name);
+		return RESULT_FAIL;
+	}
+
+	reserve = g_app_elf->evbase + 1;
+	reserve = ldrInitEventHandlersTbl(g_state_any_hdls, reserve);
+	reserve = ldrInitEventHandlersTbl(g_state_init_hdls, reserve);
+	reserve = ldrInitEventHandlersTbl(g_state_main_hdls, reserve);
+
+	status |= APP_Register(&g_app_elf->evbase, 1, g_state_table_hdls, APP_STATE_MAX, (void *) ApplicationStart);
+	if (status == RESULT_OK) {
+		PFprintf("%s: Application has been registered successfully.\n", g_app_elf->name);
+
+		status |= ldrSendEvent(g_app_elf->evbase);
+	} else {
+		PFprintf("%s: Cannot register application.\n", g_app_elf->name);
+	}
+
+	return status;
+}
+#endif
+
+static UINT32 ApplicationStart(EVENT_STACK_T *ev_st, REG_ID_T reg_id, void *reg_hdl) {
+	UINT32 status;
+	APP_INSTANCE_T *app_instance;
+
+	status = RESULT_FAIL;
+
+	if (AFW_InquireRoutingStackByRegId(reg_id) != RESULT_OK) {
+		app_instance = (APP_INSTANCE_T *) APP_InitAppData((void *) APP_HandleEvent, sizeof(APP_INSTANCE_T),
+			reg_id, 0, 1, 1, 1, 1, 0);
+
+		status = APP_Start(ev_st, &app_instance->app, APP_STATE_MAIN,
+			g_state_table_hdls, ApplicationStop, g_app_name, 0);
+
+#if defined(EP2)
+		g_app_elf.app = (APPLICATION_T *) app_instance;
+#elif defined(EPMCORE)
+		g_app_elf->app = &app_instance->app;
+#endif
+	}
+
+	return status;
+}
+
+static UINT32 ApplicationStop(EVENT_STACK_T *ev_st, APPLICATION_T *app) {
+	UINT32 status;
+
+	status = RESULT_OK;
+
+	DeleteDialog(app);
+
+	status |= APP_Exit(ev_st, app, 0);
+
+#if defined(EP1)
+	LdrUnloadELF(&Lib);
+#elif defined(EP2)
+	ldrUnloadElf();
+#elif defined(EPMCORE)
+	ldrUnloadElf(g_app_elf);
+#endif
+
+	return status;
+}
+
+static UINT32 HandleStateEnter(EVENT_STACK_T *ev_st, APPLICATION_T *app, ENTER_STATE_TYPE_T state) {
+	APP_INSTANCE_T *app_instance;
+	SU_PORT_T port;
+	CONTENT_T content;
+	UIS_DIALOG_T dialog;
+	APP_STATE_T app_state;
+	DRAWING_BUFFER_T buffer;
+	GRAPHIC_POINT_T point;
+
+	if (state != ENTER_STATE_ENTER) {
+		if (app->state != APP_STATE_MAIN) {
+			return RESULT_OK;
+		}
+	}
+
+	app_instance = (APP_INSTANCE_T *) app;
+
+	DeleteDialog(app);
+
+	port = app->port;
+	app_state = app->state;
+	dialog = DialogType_None;
+
+	memclr(&content, sizeof(CONTENT_T));
+
+	switch (app_state) {
+		case APP_STATE_MAIN:
+#if defined(EPMCORE)
+			UIS_CanvasGetDisplaySize(&point);
+#else
+			point = UIS_CanvasGetDisplaySize();
+#endif
+			buffer.w = point.x + 1;
+			buffer.h = point.y + 1;
+			buffer.buf = NULL;
+
+			app_instance->area.ulc.x = 0;
+			app_instance->area.ulc.y = 0;
+			app_instance->area.lrc.x = buffer.w;
+			app_instance->area.lrc.y = buffer.h;
+
+			dialog = UIS_CreateColorCanvas(&port, &buffer, TRUE);
+			break;
+		default:
+			break;
+	}
+
+	if (dialog == DialogType_None) {
+		return RESULT_FAIL;
+	}
+
+	app->dialog = dialog;
+
+	switch (app_state) {
+		case APP_STATE_MAIN:
+			PaintAll(ev_st, app, BENCHMARK_MAIN_SCREEN);
+			break;
+		default:
+			break;
+	}
+
+	return RESULT_OK;
+}
+
+static UINT32 DeleteDialog(APPLICATION_T *app) {
+	if (app->dialog != DialogType_None) {
+		UIS_Delete(app->dialog);
+		app->dialog = DialogType_None;
+		return RESULT_OK;
+	}
+
+	return RESULT_FAIL;
+}
+
+static UINT32 HandleEventKeyPress(EVENT_STACK_T *ev_st, APPLICATION_T *app) {
+	UINT32 status;
+	EVENT_T *event;
+	int i, base, step;
+
+	/*
+	 * Motorola V235 signals finding, thanks to @Indepth6
+	 *
+	 * RANGE phase 1:
+	 *     600...824, step is 25.
+	 * Found vibromotor signal on Key 5:
+	 *     700...724, vibromotor signal is here.
+	 * RANGE phase 2:
+	 *     700...726, step is 3.
+	 * Found vibromotor signal on Key 8:
+	 *     721...723, vibromotor signal is here.
+	 *
+	 *     700-702 - 1
+	 *     703-705 - 2
+	 *     706-708 - 3
+	 *     709-711 - 4
+	 *     712-714 - 5
+	 *     715-717 - 6
+	 *     718-720 - 7
+	 *     721-723 - 8
+	 *     724-726 - 9
+	 *
+	 * Found vibromotor signal on 723.
+	 *
+	 * Voltage vibromotor signal on 690?
+	 *     735 - 702 = 33
+	 *     723 - 33 = 690
+	 */
+
+	/*
+	 * Motorola V300 signals finding.
+	 *
+	 * Vibromotor signal is 38.
+	 * Voltage vibromotor signal is 239?
+	 */
+
+//	base = 600;
+//	step = 25;
+	base = 700;
+	step = 3;
+
+	status = RESULT_OK;
+	event = AFW_GetEv(ev_st);
+
+	APP_ConsumeEv(ev_st, app);
+
+	switch (event->data.key_pressed) {
+		case KEY_1:
+			for (i = base + step * 0; i < base + step * 1; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_2:
+			for (i = base + step * 1; i < base + step * 2; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_3:
+			for (i = base + step * 2; i < base + step * 3; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_4:
+			for (i = base + step * 3; i < base + step * 4; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_5:
+			for (i = base + step * 4; i < base + step * 5; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_6:
+			for (i = base + step * 5; i < base + step * 6; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_7:
+			for (i = base + step * 6; i < base + step * 7; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_8:
+			for (i = base + step * 7; i < base + step * 8; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		case KEY_9:
+			for (i = base + step * 8; i < base + step * 9; ++i) {
+				hPortWrite(i, 1);
+				PFprintf("Signal %d to 1.\n", i);
+				suSleep(50, NULL);
+			}
+			break;
+		default:
+			break;
+	}
+
+	return status;
+}
+
+static UINT32 HandleEventKeyRelease(EVENT_STACK_T *ev_st, APPLICATION_T *app) {
+	UINT32 status;
+	EVENT_T *event;
+
+	status = RESULT_OK;
+	event = AFW_GetEv(ev_st);
+
+	APP_ConsumeEv(ev_st, app);
+
+	switch (event->data.key_pressed) {
+		case KEY_RED:
+		case KEY_SOFT_LEFT:
+		case KEY_0:
+			status |= APP_UtilStartTimer(TIMER_FAST_TRIGGER_MS, APP_TIMER_EXIT, app);
+			break;
+		default:
+			break;
+	}
+
+	return status;
+}
+
+static UINT32 HandleEventTimerExpired(EVENT_STACK_T *ev_st, APPLICATION_T *app) {
+	EVENT_T *event;
+	APP_TIMER_T timer_id;
+
+	event = AFW_GetEv(ev_st);
+	timer_id = ((DL_TIMER_DATA_T *) event->attachment)->ID;
+
+	switch (timer_id) {
+		case APP_TIMER_EXIT:
+			/* No break here. */
+		case APP_TIMER_EXIT_FAST:
+			/* Play an exit sound using quiet speaker. */
+			DL_AudPlayTone(0x00,  0xFF);
+			return ApplicationStop(ev_st, app);
+			break;
+		default:
+			break;
+	}
+
+	return RESULT_OK;
+}
+
+static UINT32 PaintAll(EVENT_STACK_T *ev_st, APPLICATION_T *app, BENCHMARK_STATE_T state) {
+	APP_INSTANCE_T *app_instance;
+	GRAPHIC_POINT_T point;
+	COLOR_T color;
+	WCHAR *text;
+
+	app_instance = (APP_INSTANCE_T *) app;
+
+	switch (state) {
+		case BENCHMARK_MAIN_SCREEN:
+			UIS_CanvasSetFillColor(g_color_background);
+			UIS_CanvasFillRect(app_instance->area, app->dialog);
+			color.red = 0x00;
+			color.green = 0x00;
+			color.blue = 0x00;
+			color.transparent = 0x00;
+			UIS_CanvasSetForegroundColor(color);
+			color.transparent = 0xFF;
+			UIS_CanvasSetBackgroundColor(color);
+			point.x = 10;
+			point.y = 10;
+			text = L"Benchmark P2K";
+			UIS_CanvasDrawColorText(text, 0, u_strlen(text), point, 0, app->dialog);
+			point.x = 10;
+			point.y = 35;
+			text = L"Press some button!";
+			UIS_CanvasDrawColorText(text, 0, u_strlen(text), point, 0, app->dialog);
+			break;
+		default:
+			break;
+	}
+	return RESULT_OK;
+}
